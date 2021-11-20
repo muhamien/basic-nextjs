89dbf27f8281ade5ca034ceca90a9db364e30fbc
Muhammad Amien Rauf <muhammadamien23@gmail.com>
2021-11-20 23:38:36 +0700

    optimalisasi image

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,10 @@
 import Layout from '../../components/Layout';
+import Image from 'next/image';
 export default function index() {
     return (
         <Layout pageTitle="Blog"> 
             <h1 style={{paddingLeft:'64px',}}>Blog post</h1> 
+            <Image src="/profilePicture.jpg" width={150} height={150} />
         </Layout>
     )
 }   
