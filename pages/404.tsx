35eda71f038b23f9132ccbbe5d52c0fd45308adb
Muhammad Amien Rauf <muhammadamien23@gmail.com>
2021-11-20 22:10:00 +0700

    Redirect page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,15 @@
+import { useRouter } from "next/dist/client/router"
+import { useEffect } from "react";
+
 export default function Custom404() {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+            alert('Automaticly redirect to home')
+        }, 3000);
+    }, [])
     return (
         <div className="not-found">
             <h1>404</h1>
